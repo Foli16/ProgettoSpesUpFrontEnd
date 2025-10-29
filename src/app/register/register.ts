1f5201ed2195bf7b860e48eacb0fb9c02ac0d267
AmatiFederica <amati.federica1@gmail.com>
2025-10-29 14:14:18 +0100

    Grafica registrazione utente

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,27 +1,47 @@
 import { Component } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {AuthService} from '../../services/auth-service';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-register',
-  imports: [
-    FormsModule
-  ],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register.html',
-  styleUrl: './register.css'
+  styleUrls: ['./register.css']
 })
-export class Register
-{
-  username: string="";
-  email: string="";
-  password: string="";
+export class Register {
+  miaForm: FormGroup;
 
-  constructor(private serv:AuthService) {
+  constructor(private fb: FormBuilder, private serv: AuthService) {
+    this.miaForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()[\\]{};:/,._\\-+=<>?]).{8,}$'
+          ),
+        ],
+      ],
+      confirmPassword: ['', [Validators.required]],
+    });
   }
 
+  controllaPasswordUguali(): boolean {
+    const pass = this.miaForm.get('password')?.value;
+    const confirm = this.miaForm.get('confirmPassword')?.value;
+    return pass === confirm;
+  }
 
-  registra()
-  {
-    this.serv.registration(this.username, this.password,this.email);
+  registra() {
+    if (this.miaForm.valid && this.controllaPasswordUguali()) {
+      const valori = this.miaForm.value;
+      this.serv.registration(valori.username, valori.password, valori.email);
+    } else {
+      this.miaForm.markAllAsTouched();
+    }
   }
 }
