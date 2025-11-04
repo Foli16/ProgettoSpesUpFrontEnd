5150cc5c98c2c55d1e335c8e437d27f764066a66
Jasmine2399 <jas.giamberardino@gmail.com>
2025-11-04 11:05:21 +0100

    Changes by Feder

--- src/app/pagina-categoria/pagina-categoria.ts
+++ src/app/pagina-categoria/pagina-categoria.ts
@@ -2,10 +2,13 @@ import {Component} from '@angular/core';
 import {ProductService} from '../../services/product-service';
 import {ActivatedRoute} from '@angular/router';
 import {Product} from '../../model/Product';
+import {CardProdotto} from '../card-prodotto/card-prodotto';
 
 @Component({
   selector: 'app-pagina-categoria',
-  imports: [],
+  imports: [
+    CardProdotto
+  ],
   templateUrl: './pagina-categoria.html',
   styleUrl: './pagina-categoria.css'
 })
